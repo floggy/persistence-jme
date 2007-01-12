a8900abef2204421b8805398fb3399ebbe435f97
Thiago Leão Moreira <thiago.moreira@floggy.org>
2007-01-12 02:05:48 +0000

    Formating code.

--- src/main/java/net/sourceforge/floggy/persistence/WeaverTask.java
+++ src/main/java/net/sourceforge/floggy/persistence/WeaverTask.java
@@ -9,66 +9,63 @@ import org.apache.tools.ant.types.Reference;
 
 public class WeaverTask extends Task {
 
-    protected Path classpath;
+	protected Path classpath;
 
-    protected File input;
+	protected File input;
 
-    protected File output;
+	protected File output;
 
-    protected boolean generateSource;
+	protected boolean generateSource;
 
-    public void execute() throws BuildException {
-	Weaver compiler = new Weaver();
-
-	try {
-	    System.out.println(classpath);
-	    compiler.setClasspath(classpath.list());
-	    compiler.setInputFile(input);
-	    compiler.setOutputFile(output);
-	    compiler.setGenerateSource(generateSource);
-	    compiler.execute();
-	} catch (WeaverException e) {
-	    e.printStackTrace();
-	    throw new BuildException(e);
+	public void execute() throws BuildException {
+		Weaver compiler = new Weaver();
+		try {
+			compiler.setClasspath(classpath.list());
+			compiler.setInputFile(input);
+			compiler.setOutputFile(output);
+			compiler.setGenerateSource(generateSource);
+			compiler.execute();
+		} catch (WeaverException e) {
+			throw new BuildException(e);
+		}
 	}
-    }
 
-    /**
-         * Adds a reference to a classpath defined elsewhere.
-         * 
-         * @param r
-         *                a reference to a classpath
-         */
-    public void setBootClasspathRef(Reference r) {
-	if (classpath == null) {
-	    classpath = new Path(getProject());
+	/**
+	 * Adds a reference to a classpath defined elsewhere.
+	 * 
+	 * @param r
+	 *                a reference to a classpath
+	 */
+	public void setBootClasspathRef(Reference r) {
+		if (classpath == null) {
+			classpath = new Path(getProject());
+		}
+		classpath.append((Path) r.getReferencedObject());
 	}
-	classpath.append((Path) r.getReferencedObject());
-    }
 
-    /**
-         * Adds a reference to a classpath defined elsewhere.
-         * 
-         * @param r
-         *                a reference to a classpath
-         */
-    public void setClasspathRef(Reference r) {
-	if (classpath == null) {
-	    classpath = new Path(getProject());
+	/**
+	 * Adds a reference to a classpath defined elsewhere.
+	 * 
+	 * @param r
+	 *                a reference to a classpath
+	 */
+	public void setClasspathRef(Reference r) {
+		if (classpath == null) {
+			classpath = new Path(getProject());
+		}
+		classpath.append((Path) r.getReferencedObject());
 	}
-	classpath.append((Path) r.getReferencedObject());
-    }
 
-    public void setGenerateSource(boolean generateSource) {
-	this.generateSource = generateSource;
-    }
+	public void setGenerateSource(boolean generateSource) {
+		this.generateSource = generateSource;
+	}
 
-    public void setInput(File input) {
-	this.input = input;
-    }
+	public void setInput(File input) {
+		this.input = input;
+	}
 
-    public void setOutput(File output) {
-	this.output = output;
-    }
+	public void setOutput(File output) {
+		this.output = output;
+	}
 
 }
