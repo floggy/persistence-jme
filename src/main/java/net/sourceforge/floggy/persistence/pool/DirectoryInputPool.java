057ea4d05622fe16bf6ef55c8da3f31c8e7a0ed9
Thiago Rossato <thiago.rossato@floggy.org>
2007-06-08 03:55:29 +0000

    License header added to all classes. Method File.toURL() was marked as deprecated and it was replaced by File.toURI().toURL().

--- src/main/java/net/sourceforge/floggy/persistence/pool/DirectoryInputPool.java
+++ src/main/java/net/sourceforge/floggy/persistence/pool/DirectoryInputPool.java
@@ -22,49 +22,49 @@ import java.util.Vector;
 
 public class DirectoryInputPool implements InputPool {
 
-    protected File rootDirectory;
+	protected File rootDirectory;
 
-    protected Vector files;
+	protected Vector files;
 
-    public DirectoryInputPool(File directory) throws IOException {
-	this.rootDirectory = directory;
-	this.files = new Vector();
-	this.initFiles(directory);
-    }
-
-    public int getFileCount() {
-	return this.files.size();
-    }
-
-    public String getFileName(int index) {
-	return this.files.get(index).toString();
-    }
-
-    private void initFiles(File directory) throws IOException {
-	File[] files = directory.listFiles();
-
-	if (files != null) {
-	    String rootPath = this.rootDirectory.getCanonicalPath();
-
-	    for (int i = 0; i < files.length; i++) {
-		if (files[i].isDirectory()) {
-		    this.initFiles(files[i]);
-		} else if (files[i].isFile()) {
-		    String filePath = files[i].getCanonicalPath();
-
-		    String className = filePath.substring(rootPath.length());
-		    if (className.startsWith(File.separator)) {
-			className = className.substring(1);
-		    }
-
-		    this.files.add(className);
-		}
-	    }
+	public DirectoryInputPool(File directory) throws IOException {
+		this.rootDirectory = directory;
+		this.files = new Vector();
+		this.initFiles(directory);
 	}
-    }
 
-    public URL getFileURL(int index) throws IOException {
-	File file = new File(rootDirectory, getFileName(index));
-	return file.toURL();
-    }
+	public int getFileCount() {
+		return this.files.size();
+	}
+
+	public String getFileName(int index) {
+		return this.files.get(index).toString();
+	}
+
+	private void initFiles(File directory) throws IOException {
+		File[] files = directory.listFiles();
+
+		if (files != null) {
+			String rootPath = this.rootDirectory.getCanonicalPath();
+
+			for (int i = 0; i < files.length; i++) {
+				if (files[i].isDirectory()) {
+					this.initFiles(files[i]);
+				} else if (files[i].isFile()) {
+					String filePath = files[i].getCanonicalPath();
+
+					String className = filePath.substring(rootPath.length());
+					if (className.startsWith(File.separator)) {
+						className = className.substring(1);
+					}
+
+					this.files.add(className);
+				}
+			}
+		}
+	}
+
+	public URL getFileURL(int index) throws IOException {
+		File file = new File(rootDirectory, getFileName(index));
+		return file.toURI().toURL();
+	}
 }
