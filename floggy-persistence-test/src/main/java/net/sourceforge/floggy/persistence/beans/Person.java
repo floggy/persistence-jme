32db5b874edc2014c5cec07be7f977b02d3ce0b4
Thiago Leão Moreira <thiago.moreira@floggy.org>
2008-07-31 16:07:47 +0000

    Update the copyright and license term.

--- floggy-persistence-test/src/main/java/net/sourceforge/floggy/persistence/beans/Person.java
+++ floggy-persistence-test/src/main/java/net/sourceforge/floggy/persistence/beans/Person.java
@@ -1,5 +1,5 @@
 /**
- *  Copyright 2006-2008 Floggy Open Source Group
+ *  Copyright 2005-2008 Floggy Open Source Group
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
