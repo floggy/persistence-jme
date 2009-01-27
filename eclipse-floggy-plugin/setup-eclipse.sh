798c47b352ee4e661da24d982da7335bcbc78e10
Thiago Leão Moreira <thiago.moreira@floggy.org>
2009-01-27 08:23:15 +0000

    @add:;Added another version of Eclipse to test.

--- eclipse-floggy-plugin/setup-eclipse.sh
+++ eclipse-floggy-plugin/setup-eclipse.sh
@@ -2,8 +2,8 @@
 
 rm -rf eclipse-*
 
-unzip -qq ~/download/eclipse-SDK-3.3.1.1-win32.zip
-mv eclipse eclipse-SDK-3.3.1.1-win32
+unzip -qq ~/download/eclipse-SDK-3.3.2-win32.zip
+mv eclipse eclipse-SDK-3.3.2-win32
 
 unzip -qq ~/download/eclipse-java-europa-winter-win32.zip
 mv eclipse eclipse-java-europa-winter-win32
@@ -11,8 +11,14 @@ mv eclipse eclipse-java-europa-winter-win32
 unzip -qq ~/download/eclipse-SDK-3.2.2-win32.zip
 mv eclipse eclipse-SDK-3.2.2-win32
 
+tar -xzf ~/download/eclipse-SDK-3.3.2-linux-gtk.tar.gz
+mv eclipse eclipse-SDK-3.3.2-linux-gtk
+
 tar -xzf ~/download/eclipse-jee-ganymede-linux-gtk.tar.gz
 mv eclipse eclipse-jee-ganymede-linux-gtk
+
+tar -xzf ~/download/eclipse-jee-ganymede-SR1-linux-gtk.tar.gz
+mv eclipse eclipse-jee-ganymede-SR1-linux-gtk
 
 tar -xzf ~/download/eclipse-java-ganymede-linux-gtk.tar.gz
 mv eclipse eclipse-java-ganymede-linux-gtk
