030e49353cc898fbcb47f64f3a834d4018fbdd1d
Thiago Leão Moreira <thiago.moreira@floggy.org>
2009-06-14 22:59:22 +0000

    @fix:2517536;Fix the infinite loop building when you add the Floggy Nature to the Java project.

--- eclipse-floggy-plugin/src/main/java/net/sourceforge/floggy/eclipse/FloggyBuilder.java
+++ eclipse-floggy-plugin/src/main/java/net/sourceforge/floggy/eclipse/FloggyBuilder.java
@@ -15,12 +15,8 @@
  */
 package net.sourceforge.floggy.eclipse;
 
-import java.io.ByteArrayInputStream;
 import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
@@ -29,12 +25,13 @@ import javassist.NotFoundException;
 import net.sourceforge.floggy.persistence.Configuration;
 import net.sourceforge.floggy.persistence.Weaver;
 
-import org.apache.commons.io.FileUtils;
+import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IResourceDelta;
 import org.eclipse.core.resources.IncrementalProjectBuilder;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
@@ -48,6 +45,12 @@ import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 
+/**
+ * 
+ * @author Thiago Moreira
+ * @contributor Dan Murphy
+ *
+ */
 public class FloggyBuilder extends IncrementalProjectBuilder {
 
 	public static final String BUILDER_ID = "net.sourceforge.floggy.floggyBuilder";
@@ -56,16 +59,45 @@ public class FloggyBuilder extends IncrementalProjectBuilder {
 
 	public static final String RECORDSTORE_CLASS_NAME = "javax.microedition.rms.RecordStore";
 
-	protected IProject[] build(int kind, Map args, IProgressMonitor monitor)
-			throws CoreException {
+	protected IProject[] build(int kind, Map args, IProgressMonitor monitor) throws CoreException {
 		try {
 			LogFactory.getFactory().setAttribute(
 					"org.apache.commons.logging.Log",
 					EclipseLog.class.getName());
 			IProject project = getProject();
+			
+			Log log = LogFactory.getLog(FloggyBuilder.class);
 
-			if (project.hasNature(FloggyNature.NATURE_ID)) {
+			boolean buildNeeded = false;
+			IResourceDelta delta  = getDelta(project);
+			ArrayList sourceFolders = new ArrayList(1);
+
+			// Only do a build if the change is to a java source folder. It's highly unlikely that the delta will ever be null,
+			// if it is then assume that a build is required even though we don't know what changed.
+			if (delta != null && project.hasNature(JavaCore.NATURE_ID) && project.hasNature(FloggyNature.NATURE_ID)){
+				IClasspathEntry[] entries = JavaCore.create(project).getResolvedClasspath(true);
+				for (int i = 0; i < entries.length; i++) {
+					IClasspathEntry entry = entries[i];
+					if (entry.getEntryKind() == IClasspathEntry.CPE_SOURCE) {
+						sourceFolders.add(entry.getPath());
+					}
+				}
+				IResourceDelta[] changes = delta.getAffectedChildren(); 
+				for (int i = 0; i < changes.length; i++) {
+					IResourceDelta change = changes[i];
+					if (sourceFolders.contains(change.getFullPath())) {
+						buildNeeded = true;
+						log.info("Floggy build needed due to changed source folder: "+ change.getFullPath());
+						break;
+					}
+				}
+			} else if (delta == null) {
+				buildNeeded = true;
+			}
+			
+			if (buildNeeded) {
 				IJavaProject javaProject = JavaCore.create(project);
+				
 				IClasspathEntry[] entries = javaProject
 						.getResolvedClasspath(true);
 
@@ -116,13 +148,13 @@ public class FloggyBuilder extends IncrementalProjectBuilder {
 					File input = root.removeLastSegments(1).append(
 							javaProject.getOutputLocation()).toFile();
 
-					File temp = new File(root.toFile(), String.valueOf(System
-							.currentTimeMillis()));
-					FileUtils.forceMkdir(temp);
-					weaver.setOutputFile(temp);
+					IFolder floggyTemp = project.getFolder(".floggy.tmp");
+					if (!floggyTemp.exists()){
+						floggyTemp.create(IResource.DERIVED, true, monitor);
+					}
+					weaver.setOutputFile(floggyTemp.getLocation().toFile());
 					weaver.setInputFile(input);
-					weaver.setClasspath((String[]) classpathList
-							.toArray(new String[0]));
+					weaver.setClasspath((String[]) classpathList.toArray(new String[0]));
 					Configuration configuration= new Configuration();
 					configuration.setAddDefaultConstructor(addDefaultConstructor);
 					configuration.setGenerateSource(generateSource);
@@ -136,9 +168,10 @@ public class FloggyBuilder extends IncrementalProjectBuilder {
 					}
 					
 					IFolder outputLocation= project.getFolder(path);
-					copyFiles(temp, temp, outputLocation, monitor);
+					floggyTemp.refreshLocal(IResource.DEPTH_INFINITE, monitor);
+					copyFiles(floggyTemp, outputLocation, monitor);
 					outputLocation.refreshLocal(IResource.DEPTH_INFINITE, monitor);
-					FileUtils.forceDelete(temp);
+					cleanFolder(floggyTemp,monitor);
 				}
 			}
 		} catch (Exception e) {
@@ -149,71 +182,42 @@ public class FloggyBuilder extends IncrementalProjectBuilder {
 		return null;
 	}
 	
-	protected void copyFiles(final File sourceRoot, File sourceFolder, IFolder targetRoot, IProgressMonitor monitor) throws CoreException, IOException {
-		int index= sourceRoot.getAbsolutePath().length()+1;
-		File[] childrens= sourceFolder.listFiles();
-		for (int i = 0; i < childrens.length; i++) {
-			if (childrens[i].isFile()) {
-				String filePath= childrens[i].getAbsolutePath().substring(index);
-				
-				byte[] data= FileUtils.readFileToByteArray(childrens[i]);
-				InputStream is = new ByteArrayInputStream(data);
-				
-				IFolder packageFolder = createPackageFolder(filePath, targetRoot, monitor);
-				IFile outputFile = packageFolder.getFile(childrens[i].getName());
-				
-				if (!outputFile.exists()) {
-					outputFile.create(is, true, monitor);
-				} else {
-					outputFile.setContents(is, true, false, monitor);
+	private void cleanFolder(IFolder folder, IProgressMonitor monitor) throws CoreException {
+		IResource[] members = folder.members();
+		for (int i = 0; i < members.length; i++) {
+			IResource member = members[i];
+			if (member.getType()==IResource.FOLDER){
+				cleanFolder((IFolder) member, monitor);
+			} else if (member.getType()==IResource.FILE) {
+				((IFile)member).delete(true, monitor);
+			}
+		}
+	}
+
+	private void copyFiles(IFolder source, IFolder destination, IProgressMonitor monitor) throws CoreException {
+		IFolder newDestination;
+		IFile targetFile;
+		IFile sourceFile;
+		source.refreshLocal(IResource.DEPTH_ONE, monitor);
+		IResource[] resources = source.members();
+		for (int i = 0; i < resources.length; i++) {
+			IResource resource = resources[i];
+			if (resource.getType() == IResource.FOLDER){
+				String folderName = resource.getFullPath().lastSegment();
+				newDestination = destination.getFolder(folderName);
+				if (!newDestination.exists()){
+					newDestination.create(IResource.DERIVED, true, monitor);
 				}
-			} else {
-				copyFiles(sourceRoot, childrens[i], targetRoot, monitor);
+				copyFiles((IFolder) resource, newDestination, monitor);
+			} else if (resource.getType()==IResource.FILE) {
+				sourceFile = (IFile) resource;
+				targetFile = destination.getFile(sourceFile.getName());
+				if (targetFile.exists()){
+					targetFile.setContents(sourceFile.getContents(), IResource.DERIVED, monitor);
+				} else {
+					targetFile.create(sourceFile.getContents(), IResource.DERIVED, monitor);
+				}
 			}
-		}
-
+		}	
 	}
-	
-	/**
-	 * Create a folder matching the package name.
-	 * 
-	 * @param outputFolder
-	 * @param results
-	 * @return
-	 * @throws CoreException 
-	 */
-	private IFolder createPackageFolder(String filePath, IFolder outputFolder, IProgressMonitor monitor) throws CoreException {
-		String[] components = splitFilePath(filePath);
-		IFolder currentFolder = outputFolder;
-		
-		for (int i = 0; i < components.length - 1; i++) {
-			String component = components[i];
-			currentFolder = currentFolder.getFolder(component);
-			if (!currentFolder.exists()) {
-				currentFolder.create(true, true, monitor);
-			}
-		}
-
-		return currentFolder;
-	}
-	
-	private String[] splitFilePath(String filePath) {
-		List components= new LinkedList();
-		int beginIndex= -1;
-		int endIndex= 0;
-		do {
-			beginIndex++;
-			endIndex= filePath.indexOf(File.separatorChar, beginIndex+1);
-			if (endIndex == -1) {
-				endIndex= filePath.length();
-			}
-			String component= filePath.substring(beginIndex, endIndex);
-			components.add(component);
-			beginIndex= filePath.indexOf(File.separatorChar, endIndex);
-		} while (beginIndex != -1);
-		return (String[])components.toArray(new String[components.size()]);
-	}
-
-
-
 }
