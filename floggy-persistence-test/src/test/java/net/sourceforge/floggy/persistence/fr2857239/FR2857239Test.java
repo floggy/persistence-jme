527b65e43fbf338613fa5f6e3fa9a172f6a878b2
Thiago Leão Moreira <thiago.moreira@floggy.org>
2009-09-26 18:51:32 +0000

    @update;:Added the license terms and fixed the class name of FR2857239Test.

--- floggy-persistence-test/src/test/java/net/sourceforge/floggy/persistence/fr2857239/FR2857239Test.java
+++ floggy-persistence-test/src/test/java/net/sourceforge/floggy/persistence/fr2857239/FR2857239Test.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (c) 2006-2009 Floggy Open Source Group. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package net.sourceforge.floggy.persistence.fr2857239;
 
 import junit.framework.TestCase;
@@ -7,7 +22,7 @@ import net.sourceforge.floggy.persistence.beans.Person;
 import net.sourceforge.floggy.persistence.beans.animals.Bird;
 import net.sourceforge.floggy.persistence.beans.animals.Falcon;
 
-public class FR2857239 extends TestCase {
+public class FR2857239Test extends TestCase {
 	
 	public void testIt() throws Exception {
 		PersistableManager manager = PersistableManager.getInstance();
